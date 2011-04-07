1ff4ec75ec43042c36c6abe2a7c4d4e0f70da399
Peter Shangov <pshangov@yahoo.com>
2011-04-07 23:34:40 +0300

    Some syntax experiments

--- docs/sugar.pl
+++ docs/sugar.pl
@@ -21,3 +21,42 @@ sub test
 {
 
 }
+
+
+sub do_smth
+{
+	my ($self, %params) = validate
+	(
+		first  => { ... },
+		second => { ... },
+	);
+
+}
+
+sub do_smth
+{
+	my $self, %_ = params 
+	(
+		first  => { ... },
+		second => { ... },
+	);
+}
+
+sub _build_param_first
+{
+	my $self, %_ = params;
+}
+
+sub simple_sub
+{
+	my $self, %_ = params qw(first second);
+}
+
+sub doit 
+	:Private :Memoized
+	:Params(&Str *first! = _build_param_first, Int :count(number)?, ArrayRef[Sth] :collection?)
+	:Returns(Array[Str])
+{
+	...	
+}	
+
