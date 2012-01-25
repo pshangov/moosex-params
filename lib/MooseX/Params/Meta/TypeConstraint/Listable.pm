68f229e36e9b93d88dc01def0544ce0b8dca6d01
Peter Shangov <petershangov@venda.com>
2012-01-25 17:04:02 +0000

    Fix Listable.pm compilation error

--- lib/MooseX/Params/Meta/TypeConstraint/Listable.pm
+++ lib/MooseX/Params/Meta/TypeConstraint/Listable.pm
@@ -3,7 +3,8 @@ package MooseX::Params::Meta::TypeConstraint::Listable;
 use strict;
 use warnings;
 
-use base 'Moose::Meta::TypeConstraint::Parameterizable';
+use Moose;
+BEGIN { extends 'Moose::Meta::TypeConstraint::Parameterizable' }
 
 __PACKAGE__->meta->add_attribute('listable' => (
     reader => 'listable',
